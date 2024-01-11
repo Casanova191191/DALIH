3d34fc98a85b90db950d681469cb5993bb39a82c
Casanova191191 <dianacasanova02@gmail.com>
2024-01-11 08:09:03 -0500

    Archivos

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,16 +2,11 @@
 
 describe('Prueba login DALIH', () =>{
     it.only("Login DALIH", () =>{
-
-        //LOGIN
         cy.visit('https://dalih.app/login-dalih')
         cy.get('#email').type("casanova9812@gmail.com")
         cy.wait(1000)
         cy.get('#password').type("12345")         
-        cy.wait(2000)
-        cy.get('.ant-form-item-control-input-content > .ant-btn').click()    
-        
-        //CLIENTES
-        
+        cy.wait(1000)
+        cy.get('.ant-form-item-control-input-content > .ant-btn').click()        
     })
 })
